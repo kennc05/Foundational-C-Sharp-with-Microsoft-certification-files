d8abfa5add00e89aab4fcf641c7e7dbebd1120bb
Kenneth Cajigas <kenneth.cajigas@tombola.com>
2023-09-07 14:39:00 +0100

    Add work completed so far

---
+++ TestProject/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] orderIDsForInvestigation = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+foreach (var order in orderIDsForInvestigation) 
+{
+    if (order.StartsWith("B"))
+    {
+        Console.WriteLine(order);
+    }
+}
