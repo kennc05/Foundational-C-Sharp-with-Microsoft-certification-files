e9398deabed89a3797e38aab993527455c6ad104
Kenneth Cajigas <kenneth.cajigas@tombola.com>
2023-09-08 14:38:45 +0100

    https://learn.microsoft.com/en-gb/training/modules/csharp-evaluate-boolean-expressions/6-challenge-2

--- EvaluateBooleanChallenge/EvaluateBooleanChallenge/Program.cs
+++ EvaluateBooleanChallenge/EvaluateBooleanChallenge/Program.cs
@@ -1,3 +1,24 @@
-﻿Random coin = new Random();
-int flip = coin.Next(0, 2);
-Console.WriteLine((flip == 0) ? "heads" : "tails");
+﻿string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains("Admin") && level > 55)
+{
+    Console.WriteLine("Welcome, Super Admin user.");
+}
+else if (permission.Contains("Admin") && level <= 55)
+{
+    Console.WriteLine("Welcome, Admin user.");
+}
+else if (permission.Contains("Manager") && level >= 20)
+{
+    Console.WriteLine("Contact an Admin for access.");
+}
+else if (permission.Contains("Manager") && level < 20)
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+
