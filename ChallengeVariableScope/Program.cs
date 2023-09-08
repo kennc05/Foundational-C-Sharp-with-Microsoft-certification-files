b04aeab466002c7cf2bcd261ddc31fdda5633dcf
Kenneth Cajigas <kenneth.cajigas@tombola.com>
2023-09-08 15:10:25 +0100

    https://learn.microsoft.com/en-gb/training/modules/csharp-code-blocks/4-exercise-challenge-variable-scope

---
+++ ChallengeVariableScope/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] numbers = { 4, 8, 15, 16, 23, 42 };
+
+int total = 0;
+
+foreach (int number in numbers)
+{
+    total += number;
+
+    if (number == 42)
+    {
+        Console.WriteLine("Set contains 42");
+    }
+
+}
+
+Console.WriteLine($"Total: {total}");
--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,9 +1,5 @@
-﻿string[] orderIDsForInvestigation = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
-
-foreach (var order in orderIDsForInvestigation) 
+﻿bool flag = true;
+if (flag)
 {
-    if (order.StartsWith("B"))
-    {
-        Console.WriteLine(order);
-    }
+    Console.WriteLine(flag);
 }
