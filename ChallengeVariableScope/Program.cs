149fb958e483a18a26185fed5c46b1760e1f99ea
Kenneth Cajigas <kenneth.cajigas@tombola.com>
2023-09-08 16:19:14 +0100

    https://learn.microsoft.com/en-gb/training/modules/csharp-switch-case/2-exercise-switch-case

--- ChallengeVariableScope/Program.cs
+++ ChallengeVariableScope/Program.cs
@@ -1,16 +1,23 @@
-﻿int[] numbers = { 4, 8, 15, 16, 23, 42 };
+﻿int employeeLevel = 100;
+string employeeName = "John Smith";
 
-int total = 0;
+string title = "";
 
-foreach (int number in numbers)
+switch (employeeLevel)
 {
-    total += number;
-
-    if (number == 42)
-    {
-        Console.WriteLine("Set contains 42");
-    }
-
+    case 100:
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
 }
 
-Console.WriteLine($"Total: {total}");
+Console.WriteLine($"{employeeName}, {title}");
