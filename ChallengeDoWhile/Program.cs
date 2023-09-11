c70c115114da6f26826bba89fc1e6643c3125457
Kenneth Cajigas <kenneth.cajigas@tombola.com>
2023-09-11 10:55:05 +0100

    https://learn.microsoft.com/en-gb/training/modules/csharp-do-while/3-challenge

---
+++ ChallengeDoWhile/Program.cs
@@ -0,0 +1,29 @@
+﻿Random random = new Random();
+
+int heroHealth = 10;
+int monsterHealth = 10;
+
+int attack = random.Next(1, 10);
+
+do
+{
+    monsterHealth -= attack;
+    Console.WriteLine($"Monster was damaged and lost {attack} health and now has {monsterHealth} health.");
+
+    if (monsterHealth > 0)
+    {
+        heroHealth -= attack;
+        Console.WriteLine($"Hero was damaged and lost {attack} health and now has {heroHealth} health.");
+    }
+    
+} while (heroHealth > 0 && monsterHealth > 0);
+
+if (heroHealth > 0)
+{
+    Console.WriteLine("Hero wins!");
+}
+else
+{
+    Console.WriteLine("Monster wins!");
+}
+
--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,4 +1,20 @@
-﻿for (int i = 10; i >= 0; i--)
+﻿Random random = new Random();
+int current = random.Next(1, 11);
+
+do
 {
-    Console.WriteLine(i);
+    current = random.Next(1, 11);
+
+    if (current >= 8) continue;
+
+    Console.WriteLine(current);
+} while (current != 7);
+
+/*
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
 }
+Console.WriteLine($"Last number: {current}");
+*/
