410a62d334e652a4ad63ce1fe3498d5b925a6efe
Kenneth Cajigas <kenneth.cajigas@tombola.com>
2023-09-11 11:12:57 +0100

    Project 1: https://learn.microsoft.com/en-gb/training/modules/csharp-do-while/5-exercise-challenge-differentiate-while-do-statements

---
+++ ChallengeDifferentiateWhileDoStatements/Program.cs
@@ -0,0 +1,25 @@
+﻿int number;
+string? input;
+
+do
+
+{
+    Console.WriteLine("Enter an integer value between 5 and 10: ");
+    input = Console.ReadLine();
+    bool validNumber = int.TryParse(input, out number);
+    
+    if (validNumber)
+    {
+        if (number >= 5 && number <= 10) 
+            break;
+        else
+            Console.WriteLine($"You entered {input}. Please enter a number between 5 and 10.");
+    }
+    else
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    }
+
+} while (input != null);
+
+Console.WriteLine($"Your input value ({number}) has been accepted.");
