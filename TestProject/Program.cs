e92624d13e3cb44613d27ab4e0c36c02c01a3384
Kenneth Cajigas <kenneth.cajigas@tombola.com>
2023-09-22 16:41:57 +0100

    https://learn.microsoft.com/en-gb/training/modules/csharp-convert-cast/6-challenge-2

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,20 +1,14 @@
-﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+﻿int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
 
-decimal total = 0;
-string message = "";
 
-foreach (string value in values)
-{
-	decimal number;
 
-	if (decimal.TryParse(value, out number))
-	{
-		total += number;
-	}
-	else
-	{
-		message += value;
-	}
-}
+int result1 = Convert.ToInt32((decimal)value1 / value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
-Console.WriteLine($"Message: {message} \nTotal: {total}");
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
