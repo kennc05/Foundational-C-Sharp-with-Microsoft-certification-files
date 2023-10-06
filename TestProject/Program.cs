4e7c83c16ecc58ee5c9c737ee85fd08c604fa322
Kenneth Cajigas <kenneth.cajigas@tombola.com>
2023-10-06 16:49:29 +0100

    https://learn.microsoft.com/en-gb/training/modules/csharp-format-strings/6-solution

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,18 +1,30 @@
-﻿string pangram = "The quick brown fox jumps over the lazy dog";
+﻿string customerName = "Ms. Barros";
 
-string[] pangramArray = pangram.Split(' ');
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
 
-string[] panagramReversed = new string[pangramArray.Length];
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
 
-int i = 0;
-foreach (string word in pangramArray)
-{
-	char[] wordArray = word.ToCharArray();
-	Array.Reverse(wordArray);
-	panagramReversed[i] = String.Join("", wordArray);
-	i++;
-}
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
 
-string reversedPangram = string.Join(" ", panagramReversed);
+Console.WriteLine("Here's a quick comparison:\n");
 
-Console.WriteLine(reversedPangram);
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+Console.WriteLine(comparisonMessage);
