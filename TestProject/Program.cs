abe65630895810754f90939f2692ad5408835c7f
Kenneth Cajigas <kenneth.cajigas@tombola.com>
2023-10-13 14:10:29 +0100

    Part 4 unit 5 exercise challenge: https://learn.microsoft.com/en-gb/training/modules/csharp-modify-content/5-exercise-challenge-extract-replace-remove-data

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,30 +1,10 @@
-﻿string customerName = "Ms. Barros";
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
 
-string currentProduct = "Magic Yield";
-int currentShares = 2975000;
-decimal currentReturn = 0.1275m;
-decimal currentProfit = 55000000.0m;
+string quantity = "";
+string output = "";
 
-string newProduct = "Glorious Future";
-decimal newReturn = 0.13125m;
-decimal newProfit = 63000000.0m;
+quantity = "Quantity: " + input.Substring(input.IndexOf("<span>")+ 6, 4);
+output = "Output: " + input.Substring(input.IndexOf("<div>")+5, input.IndexOf("</div>") - 5).Replace("&trade;", "&reg;");
 
-Console.WriteLine($"Dear {customerName},");
-Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
-Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
-Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
-
-Console.WriteLine("Here's a quick comparison:\n");
-
-string comparisonMessage = "";
-
-comparisonMessage = currentProduct.PadRight(20);
-comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
-comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
-
-comparisonMessage += "\n";
-comparisonMessage += newProduct.PadRight(20);
-comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
-comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
-
-Console.WriteLine(comparisonMessage);
+Console.WriteLine(quantity);
+Console.WriteLine(output);
