531b2ed4431b7e01091ccebe0a528d7f20caadde
Kenneth Cajigas <kenneth.cajigas@tombola.com>
2023-10-16 12:05:44 +0100

    Add suggested donation to menu https://learn.microsoft.com/en-gb/training/modules/guided-project-work-variable-data-c-sharp/4-exercise-add-suggested-donation-data

--- Guided-project-Work-with-variable-data-in-CSharp-main/Starter/Program.cs
+++ Guided-project-Work-with-variable-data-in-CSharp-main/Starter/Program.cs
@@ -115,7 +115,7 @@ do
                 if (ourAnimals[i, 0] != "ID #: ")
                 {
                     Console.WriteLine();
-                    for (int j = 0; j < 6; j++)
+                    for (int j = 0; j < 7; j++)
                     {
                         Console.WriteLine(ourAnimals[i, j]);
                     }
