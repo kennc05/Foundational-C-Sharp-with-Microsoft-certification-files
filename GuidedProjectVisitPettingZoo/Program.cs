3414198585778c362a7cc4724fbd0941d722f7dc
Kenneth Cajigas <kenneth.cajigas@tombola.com>
2023-10-18 21:36:41 +0100

    Add initial project files

---
+++ GuidedProjectVisitPettingZoo/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+string[] pettingZoo = 
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
